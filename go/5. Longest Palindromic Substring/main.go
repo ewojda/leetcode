07e25a1c30e80399d55b7c109b81024d0d4b9a64
Emil Wojda <ewojda22@wp.pl>
2020-05-25 22:25:25 +0200

    Solution for 5.

--- go/5. Longest Palindromic Substring/main.go
+++ go/5. Longest Palindromic Substring/main.go
@@ -3,7 +3,35 @@ package main
 func main() {}
 
 func longestPalindrome(s string) string {
-	for i := range s {
-		
+	if len(s) <= 1 {
+		return s
 	}
+	longest := ""
+	longestPalindromeCenteredAt := func(left, right int) string {
+		if s[left] != s[right] {
+			return ""
+		}
+		for {
+			if left < 0 || right >= len(s) || s[left] != s[right] {
+				return s[left+1 : right]
+			}
+			left--
+			right++
+		}
+	}
+	setIfNewLongest := func(left, right int) {
+		if newLongest := longestPalindromeCenteredAt(left, right); len(newLongest) > len(longest) {
+			longest = newLongest
+		}
+	}
+	for i := 0; i < len(s)-1; i++ {
+		setIfNewLongest(i, i+1)
+	}
+	for i := 0; i < len(s)-2; i++ {
+		setIfNewLongest(i, i+2)
+	}
+	if longest == "" {
+		return s[:1]
+	}
+	return longest
 }
